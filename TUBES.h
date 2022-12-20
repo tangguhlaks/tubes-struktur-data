33f0fced2e5d979a8ef52bbc7b15492c8b1d4654
Farhan Mulya Argyanto <mulyaargyanto@gmail.com>
2022-12-20 23:51:53 +0700

    Isi header pasien

--- TUBES.h
+++ TUBES.h
@@ -5,15 +5,32 @@
 using namespace std;
 
 typedef struct jadwal_dokter *adr_dokter;
+typedef struct element_pasien *adr_pasien;
+
+struct pasien{
+	int id, biaya_dokter, biaya_obat;
+	string nama, jenis, hasil;
+};
 
 struct jadwal_dokter{
 	string tanggal,nama,spesialisasi;
 	int kuota,jam_praktek_start,jam_praktek_end;
 };
 
+struct element_pasien {
+	pasien info;
+	adr_pasien next, prev;
+
+};
+
+
 struct element_jadwal_dokter {
 	infotype info;
 	adr next;
+};
+
+struct ListPasien{
+	adr_pasien first, last;
 };
 
 struct ListJadwalDokter{
